91a788e2d4407323d2699f7eff626b290ff22ada
Adam Arafa Røe <adamarafaroe@nav.no>
2023-01-17 09:11:41 +0100

    endret navn paa separator i config filen

--- src/kafka_source.py
+++ src/kafka_source.py
@@ -34,7 +34,7 @@ class KafkaSource(Source):
     def _json_deserializer(self, message_value: bytes) -> Tuple[Dict[Text, Any], Text]:
         message = message_value.decode("UTF-8")
         dictionary = benedict(json.loads(message), keypath_separator='.')
-        separator = self.config.get("nivaa-separator")
+        separator = self.config.get("keypath-seperator")
         if separator is not None:
             dictionary.keypath_separator = separator
         filter_config = self.config.get("message-fields-filter")
@@ -79,7 +79,7 @@ class KafkaSource(Source):
         value = schema_cache[schema_id].read(decoder)
         value["kafka_message"] = value.encode("UTF-8")
         value = benedict(value, keypath_separator='.')
-        separator = self.config.get("nivaa-separator")
+        separator = self.config.get("keypath-seperator")
         if separator is not None:
             value.keypath_separator = separator
         filter_config = self.config.get("message-fields-filter")
