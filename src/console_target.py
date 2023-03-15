7043e024218cc2a7375b8abbd386f3e23ae9712f
Espen Holtebu <espen.holtebu@nav.no>
2023-03-15 15:48:46 +0100

    Legger til override på create_connection

--- src/console_target.py
+++ src/console_target.py
@@ -3,9 +3,14 @@ from base import Target
 
 
 class console_target(Target):
+
+    def create_connection(self) -> object:
+        pass
+
     def write_batch(self, batch: List[Dict[Text, Any]]) -> None:
         print(batch)
 
     def get_kode67(self, batch: List[Dict[Text, Any]]) -> List[Tuple]:
         k67_list = [(1,), (2,)]
         return k67_list
+    
