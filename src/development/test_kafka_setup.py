e62f63a75b640ffbdd3b3f0fedac631f29f30f79
Eystein Waade <eystein.waade@nav.no>
2024-12-18 09:42:50 +0100

    fjern ugyldige pytest marks

--- src/development/test_kafka_setup.py
+++ src/development/test_kafka_setup.py
@@ -2,7 +2,6 @@ import pytest
 import json
 
 
-@pytest.mark.run("first")
 def test_consumer(consumer):
     m1 = consumer.poll()
     m2 = consumer.poll()
@@ -12,14 +11,12 @@ def test_consumer(consumer):
     assert json.loads(m2.value())["id"] == 1
 
 
-@pytest.mark.run("second")
 def test_consumer_continue_at_offset(consumer):
     m3 = consumer.poll()
     consumer.close()
     assert json.loads(m3.value())["id"] == 2
 
 
-@pytest.mark.run("last")
 def test_consumer_continue_at_same_offset_after_no_commit(consumer):
     m3 = consumer.poll()
     consumer.close()
